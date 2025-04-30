bb3acb3b6b30ad73cc61161b21467f2b7cde9484
Isaac <isaacmartin.dev@gmail.com>
2025-04-30 12:55:55 +0200

    Aplicado conversor de DateTime para PostgreSQL en 'AppDbContext'

--- GameStore.CleanArch.Backend.Infrastructure/Context/AppDbContext.cs
+++ GameStore.CleanArch.Backend.Infrastructure/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using GameStore.CleanArch.Backend.Domain.Entities;
+﻿using System.Reflection;
+using GameStore.CleanArch.Backend.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
@@ -14,6 +15,27 @@ namespace GameStore.CleanArch.Backend.Infrastructure.Context
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
+            // Conversor genérico para DateTime y DateTime? UTC (con Kind=Utc)
+            var dateTimeConverter = new ValueConverter<DateTime?, DateTime?>(
+                v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : v,
+                v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : v
+            );
+
+            // Aplicar conversores automáticamente a todas las propiedades DateTime y DateTime? en todas las entidades
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                foreach (var property in entityType.ClrType.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+                {
+                    if (property.PropertyType == typeof(DateTime) || property.PropertyType == typeof(DateTime?))
+                    {
+                        modelBuilder.Entity(entityType.ClrType)
+                            .Property(property.Name)
+                            .HasConversion(dateTimeConverter);
+                    }
+                }
+            }
+
             // Conversor de DateOnly <-> DateTime
             var dateOnlyConverter = new ValueConverter<DateOnly, DateTime>(
                 d => d.ToDateTime(TimeOnly.MinValue),
